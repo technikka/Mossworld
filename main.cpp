6b63295bfc444973db085f368deaaedf9e2e9f5f
Nikki Brusoe <nikkibrusoe@gmail.com>
2026-05-28 12:13:01 -0700

    includes std namespace to remove all 'std::' prefixes

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <string>
+#include <cstdlib> // for rand()
+using namespace std;
 
 int main()
 {
@@ -15,30 +18,30 @@ int main()
 
     // Get world size from user
     // COMMENTED OUT FOR TESTING
-    // std::cout << "Select World Size\n";
-    // std::cout << "Height: (enter number 4 - 20)\n";
-    // std::cin >> world_height;
-    // std::cout << "Width: (enter number 4 - 20)\n";
-    // std::cin >> world_width;
+    // cout << "Select World Size\n";
+    // cout << "Height: (enter number 4 - 20)\n";
+    // cin >> world_height;
+    // cout << "Width: (enter number 4 - 20)\n";
+    // cin >> world_width;
 
     // Display world info
-    std::cout
+    cout
         << "\nWelcome to Mossworld.\n";
-    std::cout << "Tiny creatures stir beneath the dawn mist.\n\n";
+    cout << "Tiny creatures stir beneath the dawn mist.\n\n";
 
     // Display world status
-    std::cout << "Turn: " << turn_number << "  |  " << "Creatures: " << creature_count << "  |  " << "Food: " << food_count << "\n\n";
+    cout << "Turn: " << turn_number << "  |  " << "Creatures: " << creature_count << "  |  " << "Food: " << food_count << "\n\n";
 
     for (int row = 0; row < world_height; row++)
     {
         for (int column = 0; column < world_width; column++)
         {
-            std::cout << unoccupied_space;
-            std::cout << " ";
+            cout << unoccupied_space;
+            cout << " ";
         }
-        std::cout << std::endl;
+        cout << endl;
     }
-    std::cout << "\n\n";
+    cout << "\n\n";
 
     return 0;
 }
