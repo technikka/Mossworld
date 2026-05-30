c42c89b1bcc03a42f13890e181d2c8fe918854a3
Nikki Brusoe <nikkibrusoe@gmail.com>
2026-05-29 19:05:32 -0700

    restructures code, namely restructures main() and pulls out createTiles()

--- main.cpp
+++ main.cpp
@@ -3,47 +3,26 @@
 #include <string>
 using namespace std;
 
-int main() {
-  char unoccupied_space = '.';
-  char creature = 'k';
-  char food = '~';
+const char UNOCCUPIED_SPACE = '.';
+const char CREATURE = 'k';
+const char FOOD = '~';
 
-  int world_height = 6;
-  int world_width = 9;
+const int WORLD_HEIGHT = 6;
+const int WORLD_WIDTH = 9;
 
-  int turn_number = 1;
-  int creature_count = 3;
-  int food_count = 2;
+struct Tile {
+  int x;
+  int y;
+  char occupant;
+};
 
-  struct Tile {
-    int x;
-    int y;
-    char occupant;
-  };
-
-  // Create some randomness
-  srand(time(0));
-  int random_number;
-
-  // Get world size from user
-  // cout << "Select World Size\n";
-  // cout << "Height: (enter number 4 - 20)\n";
-  // cin >> world_height;
-  // cout << "Width: (enter number 4 - 20)\n";
-  // cin >> world_width;
-
-  // Display world info
-  cout << "\nWelcome to Mossworld.\n";
-  cout << "Tiny creatures stir beneath the dawn mist.\n\n";
-
-  // Create tiles & Print tiles
+// is also creating creatures for now
+void createTiles(int creature_count, int food_count) {
   int creatures_placed = 0;
   int food_placed = 0;
 
-  for (int row = 0; row < world_height; row++) {
-    for (int column = 0; column < world_width; column++) {
-      random_number = rand() % 10;
-
+  for (int row = 0; row < WORLD_HEIGHT; row++) {
+    for (int column = 0; column < WORLD_WIDTH; column++) {
       Tile tile;
       tile.x = column;
       tile.y = row;
@@ -51,25 +30,39 @@ int main() {
       bool max_creatures = creatures_placed == creature_count;
       bool max_food = food_placed == food_count;
 
+      int random_number = rand() % 10;
+
       if (random_number == 6 && !max_creatures) {
-        tile.occupant = creature;
+        tile.occupant = CREATURE;
         creatures_placed += 1;
       } else if (random_number == 9 && !max_food) {
-        tile.occupant = food;
+        tile.occupant = FOOD;
         food_placed += 1;
       } else {
-        tile.occupant = unoccupied_space;
+        tile.occupant = UNOCCUPIED_SPACE;
       }
       cout << ' ' << tile.occupant << ' ';
     }
     cout << endl;
   }
-  cout << "\n\n";
+}
 
-  // Display world status
+int main() {
+  int turn_number = 1;
+  int creature_count = 3;
+  int food_count = 2;
+  srand(time(0));  // seed rand
+
+  cout << "\nWelcome to Mossworld.\n";
+  cout << "Tiny creatures stir beneath the dawn mist.\n\n";
+
+  createTiles(creature_count, food_count);
+
+  cout << "\n\n";  // space beneath world
+
   cout << "Turn: " << turn_number << "  |  "
-       << "Creatures: " << creatures_placed << "  |  "
-       << "Food: " << food_placed << "\n\n";
+       << "Creatures: " << creature_count << "  |  "
+       << "Food: " << food_count << "\n\n";
 
   return 0;
 }
