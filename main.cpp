394377786d94f9e30d6804af7595dc2782b72ec6
Nikki Brusoe <nikkibrusoe@gmail.com>
2026-05-31 13:26:48 -0700

    updates functionality for creature placement
    - Added tile IDs
    - Added Position struct
    - Added spawn zones
    - Added helper functions to convert between tile id and position.
    - Fixed reference/copy issue

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <array>
 #include <cstdlib>  // for rand()
+#include <ctime>
 #include <iostream>
 #include <string>
 using namespace std;
@@ -12,16 +13,22 @@ const int WORLD_HEIGHT = 6;
 const int WORLD_WIDTH = 9;
 
 struct Tile {
+  int id;
   int x;
   int y;
   char occupant;
 };
 
+struct Position {
+  int x;
+  int y;
+};
+
 using World = array<array<Tile, WORLD_WIDTH>, WORLD_HEIGHT>;
 
 void printWorld(World& world) {
-  for (int row = 0; row < world.size(); row++) {
-    for (int column = 0; column < world[0].size(); column++) {
+  for (int row = 0; row < WORLD_HEIGHT; row++) {
+    for (int column = 0; column < WORLD_WIDTH; column++) {
       cout << ' ' << world[row][column].occupant << ' ';
     }
     cout << endl;
@@ -29,33 +36,65 @@ void printWorld(World& world) {
   cout << endl;
 }
 
+Position idToCoordinates(int tile_id) {
+  int x = tile_id % WORLD_WIDTH;
+  int y = tile_id / WORLD_WIDTH;
+  Position position;
+  position.x = x;
+  position.y = y;
+  return position;
+}
+
+int coordinatesToId(Position coordinates) {
+  return coordinates.y * WORLD_WIDTH + coordinates.x;
+}
+
 World createWorld(int creature_count, int food_count) {
-  int creatures_placed = 0;
-  int food_placed = 0;
+  // * world[0] is the first row, world[0][1] is first row, second column.
   World world;  // instantiating world array
 
+  // create tiles
   for (int row = 0; row < WORLD_HEIGHT; row++) {
     for (int column = 0; column < WORLD_WIDTH; column++) {
       Tile& tile = world[row][column];
+      tile.id = row * WORLD_WIDTH + column;
       tile.x = column;
       tile.y = row;
-
-      bool max_creatures = creatures_placed == creature_count;
-      bool max_food = food_placed == food_count;
-
-      int random_number = rand() % 10;
-
-      if (random_number == 6 && !max_creatures) {
-        tile.occupant = CREATURE;
-        creatures_placed += 1;
-      } else if (random_number == 9 && !max_food) {
-        tile.occupant = FOOD;
-        food_placed += 1;
-      } else {
-        tile.occupant = UNOCCUPIED_SPACE;
-      }
+      tile.occupant = UNOCCUPIED_SPACE;
     }
   }
+
+  // create spawn zones and randomly place a creature within each zone.
+  int number_of_tiles = (WORLD_HEIGHT * WORLD_WIDTH);
+
+  unsigned int counter = 0;
+  for (int i = 0; i < creature_count; i++) {
+    int start_id = counter;
+    // divide the tile space proportionally:
+    int end_id = ((i + 1.0) / creature_count) * number_of_tiles;
+
+    bool creature_placed = false;
+
+    // * WARNING: this loop assumes at least one empty tile exists in the zone.
+    // * Infinite-loop potential.
+    while (!creature_placed) {
+      int rand_pos = rand() % (end_id - start_id);
+
+      // find the tile to update
+      int tile_id = rand_pos + start_id;
+      Position pos = idToCoordinates(tile_id);  // p
+      Tile& tile = world[pos.y][pos.x];
+
+      // update tile
+      if (tile.occupant == UNOCCUPIED_SPACE) {
+        tile.occupant = CREATURE;
+        creature_placed = true;
+      }
+    }
+
+    counter = end_id;
+  }
+
   return world;
 }
 
