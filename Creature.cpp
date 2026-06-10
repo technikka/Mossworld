3bb84a69e154af3b03511ee5a54ee932a373095a
Nikki Brusoe <nikkibrusoe@gmail.com>
2026-06-10 15:25:49 -0700

    adds id, position, and movement functionality

--- Creature.cpp
+++ Creature.cpp
@@ -2,24 +2,31 @@
 using namespace std;
 
 #include "Creature.h"
+#include "Position.h"
 
-Creature::Creature(CreatureType type) { this->type = type; }
+Creature::Creature(CreatureType type, int id, Position position) {
+    this->type = type;
+    this->id = id;
+    this->position = position;
+}
 
 char CreatureTypeToSymbol(CreatureType type) {
-  switch (type) {
-    case MOSSLING:
-      return 'k';
-  }
-  return '?';  // should never be reached
+    switch (type) {
+        case MOSSLING:
+            return 'k';
+    }
+    return '?';  // should never be reached
 }
 
 string CreatureTypeToString(CreatureType type) {
-  switch (type) {
-    case MOSSLING:
-      return "Mossling";
-  }
-  return "?";  // should never be reached
+    switch (type) {
+        case MOSSLING:
+            return "Mossling";
+    }
+    return "?";  // should never be reached
 }
+
+int Creature::GetId() { return id; }
 
 string Creature::GetTypeString() { return CreatureTypeToString(this->type); }
 char Creature::GetTypeSymbol() { return CreatureTypeToSymbol(this->type); }
--- Creature.h
+++ Creature.h
@@ -1,4 +1,6 @@
 #include <string>
+
+#include "Position.h"
 using namespace std;
 #ifndef CREATURE_H
 #define CREATURE_H
@@ -7,13 +9,16 @@ using namespace std;
 enum CreatureType { MOSSLING };
 
 class Creature {
- public:
-  Creature(CreatureType type);
-  string GetTypeString();
-  char GetTypeSymbol();
+   public:
+    Creature(CreatureType type, int id, Position position);
+    string GetTypeString();
+    char GetTypeSymbol();
+    int GetId();
+    int id;
+    Position position;
 
- private:
-  CreatureType type;
+   private:
+    CreatureType type;
 };
 
 #endif
