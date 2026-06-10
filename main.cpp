2333397f9c9aaad066a60b8b80215dabea6ba7c2
Nikki Brusoe <nikkibrusoe@gmail.com>
2026-06-10 15:22:20 -0700

    creates loop which prompts user to advance day, then updates world.

--- main.cpp
+++ main.cpp
@@ -9,16 +9,32 @@ using namespace std;
 #include "World.h"
 
 int main() {
-    int turn_number = 1;
+    int day = 1;
     srand(time(0));  // seed rand
 
     cout << "\nWelcome to Mossworld.\n";
     cout << "Tiny creatures stir among the morning dew.\n\n";
 
-    // World creates tiles and creatures
-    World world(3, 2);
-    world.print();
-    world.printHUD(turn_number);
+    World world(3, 2);  // instantiate world
+
+    bool sim_continue = false;
+    do {
+        world.print();
+        world.printHUD(day);
+
+        cout << "\nA new day is unfolding. Observe? (y/n)\n" << endl;
+
+        char input;
+        cin >> input;
+
+        if (input == 'y') {
+            sim_continue = true;
+            day += 1;
+            world.advanceDay();
+        } else {
+            sim_continue = false;
+        }
+    } while (sim_continue == true);
 
     return 0;
 }
